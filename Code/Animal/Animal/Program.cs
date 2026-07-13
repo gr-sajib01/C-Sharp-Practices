f323345293974cf8b0feba0a33a64ac68bdc1b43
Md Golam Rabbani Sajib <grsajib600@gmail.com>
2026-07-14 02:52:59 +0600

    Create Program.cs

---
+++ Code/Animal/Animal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Animal
+{
+    class Animal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal makes sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Cat meows");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            Dog dog1 = new Dog();
+            Cat cat1 = new Cat();
+
+            animal1.MakeSound();
+            dog1.MakeSound();
+            cat1.MakeSound();
+        }
+    }
+}
